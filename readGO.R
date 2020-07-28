5534d6e50b3907fc9f4c74a7f97fe3a3ec26a9a8
Yasir Suhail <yasir.suhail@gmail.com>
2020-07-28 18:22:54 -0400

    read in go

--- readGO.R
+++ readGO.R
@@ -1,3 +1,30 @@
-library(biomaRt)
+library(geneListPie)
+library(data.table)
+data("goslim.human.all")
 
-gaf <- read.table(file = "data/goa_human.gaf", header = T, comment.char = "!", sep = "\t", fill = F, quote = '"')
+aa <- read.table(file = "data/BIOGRID-MV-Physical-3.5.187.tab3.txt", sep = "\t", quote = '"', comment.char = "", header = T)
+aa <- as.data.table(aa)
+aa = aa[Organism.Interactor.A == 9606][Organism.Interactor.B== 9606]
+allgenes = unique(c(aa$Official.Symbol.Interactor.A, aa$Official.Symbol.Interactor.B))
+allgenes.upper = unique(toupper(allgenes))
+
+num.gs = length(goslim.human.all)
+slimlist = lapply(goslim.human.all, FUN = function(xx) {
+  line = strsplit(xx, "\t")[[1]]
+  sym = toupper(line[3:length(line)])
+  sym
+})
+
+
+names(slimlist) = sapply(goslim.human.all, FUN = function(xx) {
+  strsplit(xx, "\t")[[1]][1]
+})
+
+
+allgenes2memberships = sapply(X = allgenes.upper, FUN = function(gg) {
+  sapply(X = slimlist, FUN = function(glist) {
+    (gg %in% glist)
+  })
+})
+
+write.csv(allgenes2memberships, "gene_goslim_matrix.csv")
